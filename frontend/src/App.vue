0e2f195d0b12750bdd7e06c69c26bd8bd4b3b7f2
Maurício Casagrande <mauricio.araras@hotmail.com>
2023-01-18 12:59:15 -0300

    att

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <create-component/>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CreateComponent from "@/components/CreateComponent/CreateComponent.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CreateComponent
   }
 }
 </script>
@@ -21,6 +22,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
